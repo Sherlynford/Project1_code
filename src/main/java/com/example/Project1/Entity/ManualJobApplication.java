e65e2de881b7e617d7f11945941776f885547533
Sherlynford <bobowba2547@gmail.com>
2024-08-16 18:34:15 +0700

    Revert "small fix"
    This reverts commit 058cc2d89f8ff3d8ffcbcc401b68fbec495e1b42.

--- src/main/java/com/example/Project1/Entity/ManualJobApplication.java
+++ src/main/java/com/example/Project1/Entity/ManualJobApplication.java
@@ -43,9 +43,9 @@ public class ManualJobApplication {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate applicationDateTime;
+    private LocalDate ApplicationDateTime;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
-    private LocalDateTime interviewDate;
+    private LocalDateTime InterviewDate;
 }
